d8b0c208441cffa4eb8972dab662ef3702672376
Juan Jose <juanjo2003gg@gmail.com>
2024-03-22 04:51:27 -0500

    Restriccion de kg quitada

--- src/components/AgregarGrua/AgregarGrua.jsx
+++ src/components/AgregarGrua/AgregarGrua.jsx
@@ -64,10 +64,6 @@ function AgregarGrua() {
       setErrorMensaje("Complete todos los campos.");
       return;
     }
-    if (!gruaInfo.modelo.trim() || !gruaInfo.capacidad.trim() || !gruaInfo.ubicacion.trim()) {
-      setErrorMensaje("Complete todos los campos.");
-      return;
-    }
     if (!/^\d+$/.test(gruaInfo.modelo)) {
       setErrorMensaje("Por favor, ingrese solo números en el campo Modelo.");
       return;
